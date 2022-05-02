c33524b54098137ecf84146ef1f610f31fa6e0a3
apiekarz <czasula@gmail.com>
2022-05-02 13:10:14 +0200

    Some beautifying

--- equation.py
+++ equation.py
@@ -33,28 +33,7 @@ class Equation:
         return ""
 
 
-# example equation 1
-class Sine(Equation):
-    def __init__(self):
-        super().__init__()
-        self.xlim = (0, 4*np.pi)
-        self.ylim = (-np.pi, np.pi)
-
-    def data_gen(self):
-        for cnt in itertools.count():
-            x = cnt / 20
-            y = np.sin(2 * np.pi * (x - 0.01 * x))
-            yield x, y
-
-    def __str__(self):
-        return "Sine wave"
-
-    @staticmethod
-    def text_equation():
-        return "sin(X)"
-
-
-# example equation 2
+# example equation
 class TripleSine(Equation):
     def __init__(self):
         super().__init__()
@@ -76,7 +55,8 @@ class TripleSine(Equation):
 
     @staticmethod
     def text_equation():
-        return "sin(x) + sin(x+2) + sin(x/2)"
+        return "Triple sine wave (example)\n" \
+               "sin(x) + sin(x+a) + sin(x/b)"
 
 
 class LorenzSystem(Equation):
--- window.py
+++ window.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 from ttkthemes import ThemedTk
 import matplotlib.animation as animation
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from equation import Sine, TripleSine, LorenzSystem, RosslerSystem, LotkaVolterra
+from equation import TripleSine, LorenzSystem, RosslerSystem, LotkaVolterra
 
 
 class Window:
@@ -14,12 +14,13 @@ class Window:
         self.canvas = FigureCanvasTkAgg(plot.fig, master=self.root)
 
         self.combobox = ttk.Combobox(self.root, width=34)
+        self.param_label = tk.Label(self.root, text="Choose a parameter to change:")
         self.param_combo = ttk.Combobox(self.root, width=8)
         self.param_value = tk.Entry(self.root, width=10)
-        self.apply = tk.Button(self.root, text="apply")
+        self.apply = tk.Button(self.root, text=" apply ")
 
         self.equation = tk.Label(self.root, text="Choose something")
-        self.pause = tk.Button(self.root, text="pause")
+        self.pause = tk.Button(self.root, text=" pause ")
         self.paused = False
 
         self.add_options_to_list()
@@ -32,24 +33,34 @@ class Window:
                                            interval=20, blit=False, cache_frame_data=False)
 
     def beautify(self):
+        unified_font = ("Consolas", 14)
+        button_font = ("Consolas", 12)
+
         self.root['padx'] = 20
         self.root['background'] = 'white'
         self.label['background'] = 'white'
+        self.label['font'] = unified_font
         self.equation['background'] = 'white'
+        self.equation['font'] = unified_font
+        self.param_label['background'] = 'white'
+        self.param_label['font'] = unified_font
+        self.combobox['font'] = button_font
+        self.pause['font'] = button_font
+        self.apply['font'] = button_font
 
     def place_components(self):
         self.label.grid(column=0, row=0)
-        self.canvas.get_tk_widget().grid(column=0, row=1, rowspan=4)
+        self.canvas.get_tk_widget().grid(column=0, row=1, rowspan=5)
         self.combobox.grid(column=1, columnspan=3, row=1)
-        self.param_combo.grid(column=1, row=2)
-        self.param_value.grid(column=2, row=2)
-        self.apply.grid(column=3, row=2)
-        self.equation.grid(column=1, columnspan=3, row=3)
-        self.pause.grid(column=1, row=4)
+        self.param_label.grid(column=1, columnspan=3, row=2, sticky='S')
+        self.param_combo.grid(column=1, row=3)
+        self.param_value.grid(column=2, row=3)
+        self.apply.grid(column=3, row=3)
+        self.equation.grid(column=1, columnspan=3, row=4)
+        self.pause.grid(column=1, row=5)
 
     def add_options_to_list(self):
-        self.combobox['values'] = ("sin(x)",
-                                   "triple sine",
+        self.combobox['values'] = ("triple sine",
                                    "Lorenz system",
                                    "Rössler system",
                                    "Lotka-Volterra equations")
@@ -79,12 +90,9 @@ class Window:
             self.plot.equation.params[self.param_combo.get()] = new_param
             self.plot.equation.set_initial_conditions()
             self.ani.frame_seq = self.plot.equation.data_gen()
-            print(f'new param: {new_param}')
 
     def update_equation(self, event):
-        if self.combobox.get() == "sin(x)":
-            self.plot.new_equation(Sine())
-        elif self.combobox.get() == "Lorenz system":
+        if self.combobox.get() == "Lorenz system":
             self.plot.new_equation(LorenzSystem())
         elif self.combobox.get() == "Rössler system":
             self.plot.new_equation(RosslerSystem())
